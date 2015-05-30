0bf1d9a7705abc7eb3b0131e1ca850fa42f664cc
Tysonzero <danielspencersmith@gmail.com>
2015-05-29 23:43:05 -0600

    Setup basic ANN struct

--- ann.c
+++ ann.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+#define INPUTS 3
+#define HIDDEN 5
+#define OUTPUTS 2
+#define ROWS 5
+
+typedef struct {
+    double input[HIDDEN][INPUTS];
+    double hidden[ROWS - 3][HIDDEN][HIDDEN];
+    double output[OUTPUTS][HIDDEN];
+} Links;
+
+typedef struct {
+    Links weights;
+} ANN;
+
 int main(void)
 {
     return 0;
