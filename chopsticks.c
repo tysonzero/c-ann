ebed0fe0d3ea24480ceecd62e00d395ba2cb63a1
Tysonzero <danielspencersmith@gmail.com>
2015-07-04 19:04:59 -0600

    Create chopsticks_create function

--- chopsticks.c
+++ chopsticks.c
@@ -3,6 +3,14 @@ typedef struct {
     int turn;
 } Chopsticks;
 
+void chopsticks_create(Chopsticks *chopsticks) {
+    chopsticks->fingers[0][0] = 1;
+    chopsticks->fingers[0][1] = 1;
+    chopsticks->fingers[1][0] = 1;
+    chopsticks->fingers[1][1] = 1;
+    chopsticks->turn = 0;
+}
+
 int main(void) {
 
 }
