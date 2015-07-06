3b737ace295d7ba8ce87dca821c577b04c4a61c9
Tysonzero <danielspencersmith@gmail.com>
2015-07-05 23:54:04 -0600

    Rename fingers to hands
    The value is the number of fingers, not the finger itself.

--- sticks.c
+++ sticks.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 typedef struct {
-    int fingers[2][2];
+    int hands[2][2];
     int turn;
 } Sticks;
 
 void sticks_create(Sticks *sticks) {
-    sticks->fingers[0][0] = 1;
-    sticks->fingers[0][1] = 1;
-    sticks->fingers[1][0] = 1;
-    sticks->fingers[1][1] = 1;
+    sticks->hands[0][0] = 1;
+    sticks->hands[0][1] = 1;
+    sticks->hands[1][0] = 1;
+    sticks->hands[1][1] = 1;
     sticks->turn = 0;
 }
 
 int main(void) {
     Sticks sticks;
     sticks_create(&sticks);
-    printf("%d\n", sticks.fingers[0][0]);
+    printf("%d\n", sticks.hands[0][0]);
     printf("%d\n", sticks.turn);
 }
