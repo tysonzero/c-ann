4dfcccb1252ca258c785d3ab3a95047547463de8
Tysonzero <danielspencersmith@gmail.com>
2015-07-06 23:47:59 -0600

    Create and use basic sticks_play function

--- sticks.c
+++ sticks.c
@@ -13,9 +13,17 @@ void sticks_create(Sticks *sticks) {
     sticks->turn = 0;
 }
 
+void sticks_play(Sticks *sticks, int actor, int target) {
+    sticks->hands[!sticks->turn][target] += sticks->hands[sticks->turn][actor];
+    sticks->turn = !sticks->turn;
+}
+
 int main(void) {
     Sticks sticks;
     sticks_create(&sticks);
     printf("%d\n", sticks.hands[0][0]);
+    printf("%d\n", sticks.turn);
+    sticks_play(&sticks, 0, 1);
+    printf("%d\n", sticks.hands[1][1]);
     printf("%d\n", sticks.turn);
 }
