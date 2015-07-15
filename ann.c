2934aad85487af6395dc7cd05858707b518ea311
Tysonzero <danielspencersmith@gmail.com>
2015-07-14 20:33:10 -0600

    Replace declarations and types with header include

--- ann.c
+++ ann.c
@@ -2,28 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define INPUTS 3
-#define HIDDEN 5
-#define OUTPUTS 2
-#define ROWS 5
-
-typedef struct {
-    double input[HIDDEN][INPUTS + 1];
-    double hidden[ROWS - 3][HIDDEN][HIDDEN + 1];
-    double output[OUTPUTS][HIDDEN + 1];
-} Links;
-
-typedef struct {
-    int input[INPUTS];
-    int hidden[ROWS - 2][HIDDEN];
-    int output[OUTPUTS];
-} Neurons;
-
-typedef struct {
-    Links weights;
-    Links slopes;
-    Neurons values;
-} ANN;
+#include "ann.h"
 
 void ann_create(ANN *ann) {
     int i;
