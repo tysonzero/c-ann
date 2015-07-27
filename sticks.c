d048f88c8f8bf43ae4cc7e8ee3af14ded8029f85
Tysonzero <danielspencersmith@gmail.com>
2015-07-26 23:37:38 -0600

    Check other hand rather than opponent's hand

--- sticks.c
+++ sticks.c
@@ -25,7 +25,7 @@ void sticks_play(Sticks *sticks, int attack, int x, int y) {
         int desired = 2 * x + y;
         if (desired < fingers - 4) desired = fingers - 4;
         if (desired >= sticks->hands[sticks->turn][0]) desired += 1;
-        if (desired == sticks->hands[!sticks->turn][0]) desired += 1;
+        if (desired == sticks->hands[sticks->turn][1]) desired += 1;
         if (desired > fingers) desired = fingers;
         sticks->hands[sticks->turn][0] = desired;
         sticks->hands[sticks->turn][1] = fingers - desired;
